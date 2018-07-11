5cebd398cf118bb1e3e22a1ac4aa1db9d30e12d7
MikeCAT <mikecat@users.noreply.github.com>
2018-07-11 17:16:02 +0900

    move command processing into another file

---
+++ tools/fat_command.h
@@ -0,0 +1,8 @@
+#ifndef FAT_COMMAND_H_GUARD_44BE5805_F245_4FB6_95FD_B6C1B6BE7D10
+#define FAT_COMMAND_H_GUARD_44BE5805_F245_4FB6_95FD_B6C1B6BE7D10
+
+#include "fat.h"
+
+void fat_process_command(FATINFO* fi, int argc, char* argv[]);
+
+#endif
--- tools/fattool.c
+++ tools/fattool.c
@@ -4,41 +4,7 @@
 #include "disk.h"
 #include "fat.h"
 #include "number_io.h"
-
-void fat_command_process(FATINFO* fi, int argc, char* argv[]) {
-	if (argc >= 1) {
-		if (strcmp(argv[0], "info") == 0) {
-			fat_printinfo(fi);
-		} else if (strcmp(argv[0], "ls") == 0) {
-			if (argc >= 2) {
-				FATFILE* ff = fat_openfile(fi, NULL, argv[1], FATFILE_WILL_READ);
-				if (ff == NULL) {
-					fprintf(stderr, "failed to open %s\n", argv[1]);
-				} else {
-					int attr = fat_getfileattr(ff);
-					if (attr < 0) {
-						fprintf(stderr, "failed to get attribute\n");
-					} else if (attr & FATFILE_ATTR_DIR) {
-						char name[4096];
-						if (fat_dirbegin(ff)) {
-							while (fat_dirnext(ff, name, sizeof(name)) && name[0] != '\0') {
-								printf("%s\n", name);
-							}
-							fat_dirend(ff);
-						} else {
-							fprintf(stderr, "fat_dirbegin() failed\n");
-						}
-					} else {
-						printf("%s\n", argv[1]);
-					}
-					fat_closefile(ff);
-				}
-			}
-		}
-	} else {
-		puts("no command specified.\n");
-	}
-}
+#include "fat_command.h"
 
 int main(int argc, char* argv[]) {
 	char** command = argv + argc;
@@ -184,7 +150,7 @@ int main(int argc, char* argv[]) {
 		}
 		fi = fat_open(disk, sector_start, sector_num);
 		if (fi != NULL) {
-			fat_command_process(fi, command_num, command);
+			fat_process_command(fi, command_num, command);
 			fat_close(fi);
 		}
 	}

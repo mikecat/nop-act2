0b9a2ab64569bb7b303952c2bbe25fae8c71ee78
MikeCAT <mikecat@users.noreply.github.com>
2018-07-19 22:54:20 +0900

    support Windows in disk.c

--- tools/disk.c
+++ tools/disk.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/types.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#endif
 #include "disk.h"
 
 #define SECTOR_SIZE 512
@@ -11,7 +16,11 @@
 #define DISK_CACHE_SIZE 64
 
 struct disk_t {
+#ifdef _WIN32
+	HANDLE hFile;
+#else
 	int fd;
+#endif
 	size_t size;
 
 	ssize_t cached_sector[DISK_CACHE_SIZE];
@@ -38,12 +47,26 @@ static int flash_cache(DISK* disk) {
 	if (disk == NULL) return 0;
 	for (i = 0; i < DISK_CACHE_SIZE; i++) {
 		if (disk->cache_dirty[i]) {
+#ifdef _WIN32
+			off_t seekTo = disk->cached_sector[i] * (off_t)SECTOR_SIZE;
+			LONG seekLow = seekTo & 0xffffffffu, seekHigh = (seekTo >> 16) >> 16;
+			DWORD sizeWritten = 0;
+			if (SetFilePointer(disk->hFile, seekLow, &seekHigh, FILE_BEGIN) == INVALID_SET_FILE_POINTER &&
+			GetLastError() != NO_ERROR) {
+				return 0;
+			}
+ 			if (!WriteFile(disk->hFile, disk->cache[i], SECTOR_SIZE, &sizeWritten, NULL) ||
+ 			sizeWritten != SECTOR_SIZE) {
+ 				return 0;
+ 			}
+ #else
 			if (lseek(disk->fd, disk->cached_sector[i] * (off_t)SECTOR_SIZE, SEEK_SET) == (off_t)-1) {
 				return 0;
 			}
 			if (write(disk->fd, disk->cache[i], SECTOR_SIZE) != SECTOR_SIZE) {
 				return 0;
 			}
+#endif
 			disk->cache_dirty[i] = 0;
 		}
 	}
@@ -84,27 +107,74 @@ static char* get_cache(DISK* disk, ssize_t sector, enum dirty_level dirty_flag) 
 	disk->cache_order[i] = 0;
 	if (disk->cache_dirty[i]) {
 		/* write updated data into file */
+#ifdef _WIN32
+		off_t seekTo = disk->cached_sector[i] * (off_t)SECTOR_SIZE;
+		LONG seekLow = seekTo & 0xffffffffu, seekHigh = (seekTo >> 16) >> 16;
+		DWORD sizeWritten = 0;
+		if (SetFilePointer(disk->hFile, seekLow, &seekHigh, FILE_BEGIN) == INVALID_SET_FILE_POINTER &&
+		GetLastError() != NO_ERROR) {
+			return NULL;
+		}
+		if (!WriteFile(disk->hFile, disk->cache[i], SECTOR_SIZE, &sizeWritten, NULL) ||
+		sizeWritten != SECTOR_SIZE) {
+			return NULL;
+		}
+#else
 		if (lseek(disk->fd, disk->cached_sector[i] * (off_t)SECTOR_SIZE, SEEK_SET) == (off_t)-1) {
 			return NULL;
 		}
 		if (write(disk->fd, disk->cache[i], SECTOR_SIZE) != SECTOR_SIZE) return NULL;
+#endif
 	}
 	disk->cache_dirty[i] = (dirty_flag != CLEAN);
 	disk->cached_sector[i] = sector;
 	if (dirty_flag != DIRTY_ALL) {
 		/* read new data from file */
+#ifdef _WIN32
+		off_t seekTo = disk->cached_sector[i] * (off_t)SECTOR_SIZE;
+		LONG seekLow = seekTo & 0xffffffffu, seekHigh = (seekTo >> 16) >> 16;
+		DWORD sizeRead = 0;
+		if (SetFilePointer(disk->hFile, seekLow, &seekHigh, FILE_BEGIN) == INVALID_SET_FILE_POINTER &&
+		GetLastError() != NO_ERROR) {
+			return NULL;
+		}
+		if (!ReadFile(disk->hFile, disk->cache[i], SECTOR_SIZE, &sizeRead, NULL) ||
+		sizeRead != SECTOR_SIZE) {
+			return NULL;
+		}
+#else
 		if (lseek(disk->fd, disk->cached_sector[i] * (off_t)SECTOR_SIZE, SEEK_SET) == (off_t)-1) {
 			return NULL;
 		}
 		if (read(disk->fd, disk->cache[i], SECTOR_SIZE) != SECTOR_SIZE) return NULL;
+#endif
 	}
 	return disk->cache[i];
 }
 
 DISK* open_disk(const char* disk_file) {
+#ifdef _WIN32
+	DWORD sizeLow, sizeHigh = 0;
+#else
 	struct stat st;
+#endif
 	DISK* disk = malloc(sizeof(*disk));
 	if (disk == NULL) return NULL;
+#ifdef _WIN32
+	disk->hFile = CreateFileA(disk_file, GENERIC_READ | GENERIC_WRITE, 0, NULL,
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (disk->hFile == INVALID_HANDLE_VALUE) {
+		free(disk);
+		return NULL;
+	}
+	sizeLow = GetFileSize(disk->hFile, &sizeHigh);
+	if (sizeLow == (DWORD)-1 && GetLastError() != NO_ERROR) {
+		CloseHandle(disk->hFile);
+		free(disk);
+		return NULL;
+	}
+	disk->size = sizeLow | (((size_t)sizeHigh << 16) << 16);
+#else
 	if ((disk->fd = open(disk_file, O_RDWR)) == -1) {
 		int e = errno;
 		free(disk);
@@ -119,8 +189,13 @@ DISK* open_disk(const char* disk_file) {
 		return NULL;
 	}
 	disk->size = st.st_size;
+#endif
 	if (!clear_cache(disk)) {
+#ifdef _WIN32
+		CloseHandle(disk->hFile);
+#else
 		close(disk->fd);
+#endif
 		free(disk);
 		return NULL;
 	}
@@ -130,7 +205,11 @@ DISK* open_disk(const char* disk_file) {
 int close_disk(DISK* disk) {
 	if (disk == NULL) return 0;
 	if (!flash_cache(disk)) return 0;
+#ifdef _WIN32
+	if (!CloseHandle(disk->hFile)) return 0;
+#else
 	if (close(disk->fd) == -1) return 0;
+#endif
 	free(disk);
 	return 1;
 }
